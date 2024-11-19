80ea9cde6309446dbc2eea04c3d7811e0905dee5
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-11-19 11:17:19 +0100

    shalalla

---
+++ Nasan2/Program.cs
@@ -0,0 +1,34 @@
+﻿string [] leksaker = {"Bil", "Pistol", "Såpbubblor", "Stressboll", "Skibidi"};
+
+string [] klasskompisar = {"Leonard", "David", "Rasmus", "Adam", "Eijvan"};
+
+int [] värden = {0, 3, 5, 6, 9};
+
+for (int i = 0; i < klasskompisar.Length; i++)
+{
+    Console.WriteLine($"{klasskompisar[i]} ger betyget {värden[i]} till {leksaker[i]}");
+}
+
+List<string> cities = new List<string>();
+
+while(true)
+{
+    Console.WriteLine("Skriv en stad.");
+    string city = Console.ReadLine();
+
+    if (city == "exit")
+    {
+        break;
+    }
+    else
+    {
+        cities.Add(city);
+    }
+}
+
+foreach (string stad in cities)
+{
+    Console.WriteLine(stad);
+}
+
+Console.ReadLine();
